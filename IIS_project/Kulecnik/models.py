63e881084086845545f634d96bcd44d31a5beead
Martin <martintrejtnar@atlas.cz>
2019-10-23 15:12:58 +0200

    team edit

--- IIS_project/Kulecnik/models.py
+++ IIS_project/Kulecnik/models.py
@@ -10,3 +10,6 @@ class Player(models.Model):
     email = models.EmailField(blank=True, null=True)
     photo = models.FileField(blank=True, null=True)
 
+class Team(models.Model):
+    name = models.CharField(max_length=20)
+    logo = models.FileField(blank=True, null=True)
