6558bfabdfb5273026818a8c72362c34612b5296
Jan Vaculik <vacuulikjan@gmail.com>
2019-10-23 16:29:26 +0200

    ..

--- IIS_project/Kulecnik/forms.py
+++ IIS_project/Kulecnik/forms.py
@@ -2,7 +2,7 @@ from django.forms import ModelForm, Form
 from django import forms
 from .models import Tournament_S
 
-class NewTournament_S(Form):
+class NewTournament_S(forms.Form):
 
     title = forms.CharField(max_length=50)
         
