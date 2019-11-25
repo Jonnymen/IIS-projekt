6a575d6d47d3b5be8d892a37654f7ac16152075e
Jan Vaculik <vaculikjan@gmail.com>
2019-11-25 17:21:17 +0100

    -

--- IIS_project/Kulecnik/forms.py
+++ IIS_project/Kulecnik/forms.py
@@ -1,6 +1,6 @@
 from django.forms import ModelForm, Form
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
 from .models import Tournament_S
 
@@ -19,6 +19,16 @@ class RegistrationForm(UserCreationForm):
             'email',
             'password1',
             'password2'
+        )
+
+class EditProfileForm(UserChangeForm):
+
+    class Meta():
+        model = User
+        fields = (
+            'first_name',
+            'last_name',
+            'email',
         )
 
 class LoginForm(forms.Form):
