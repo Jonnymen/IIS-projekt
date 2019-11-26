d0f3b200eda7171fd98ae461b330dffc36a5dc18
Jan Vaculik <vaculikjan@gmail.com>
2019-11-27 00:43:09 +0100

    Fixing signals

--- IIS_project/Kulecnik/signals.py
+++ IIS_project/Kulecnik/signals.py
@@ -4,10 +4,10 @@ from django.db.models.signals import User
 from .models import Profile
 
 @receiver(post_save, sender=User)
-def create_profile_for_user(sender, instance, created, **kwargs):
+def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
-def save_profile_for_user(sender, instance, **kwargs):
+def save_profile(sender, instance, **kwargs):
     instance.profile.save()
