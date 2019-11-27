9f6f759d7c14189644bfc9d549300504ac873b5b
Jan Vaculik <vaculikjan@gmail.com>
2019-11-27 01:29:31 +0100

    -

--- IIS_project/Kulecnik/models.py
+++ IIS_project/Kulecnik/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User, AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 # Create your models here.
@@ -70,3 +72,7 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, **kwargs):
+    Profile.objects.create(user=instance)
