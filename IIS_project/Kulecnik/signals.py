1cc8fb1504ea08b6101c081c98ea358fe040d8a4
Jan Vaculik <vaculikjan@gmail.com>
2019-11-27 01:22:58 +0100

    -

--- IIS_project/Kulecnik/signals.py
+++ IIS_project/Kulecnik/signals.py
@@ -1,6 +1,6 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-from django.db.models.signals import User
+from django.contrib.auth.models import User
 from .models import Profile
 
 @receiver(post_save, sender=User)
@@ -11,4 +11,3 @@ def create_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
-    
