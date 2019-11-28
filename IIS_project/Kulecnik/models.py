c60cc77fce7bb8da2655fc90eededd8c02e04cb8
Jan Vaculik <vaculikjan@gmail.com>
2019-11-28 19:42:03 +0100

    Fixes

--- IIS_project/Kulecnik/models.py
+++ IIS_project/Kulecnik/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import User, AbstractUser
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-
 # Create your models here.
 class Table(models.Model):
     name = models.CharField(max_length=30)
