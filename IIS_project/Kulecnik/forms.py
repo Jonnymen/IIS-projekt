bad3329293cf5674b4ff5797f2f2a8d0982a4068
Martin <martintrejtnar@atlas.cz>
2019-11-29 16:28:44 +0100

    DropDown

--- IIS_project/Kulecnik/forms.py
+++ IIS_project/Kulecnik/forms.py
@@ -124,7 +124,7 @@ class AddTournamentFormT(forms.ModelForm):
             'end_date': forms.TextInput(attrs={'rows':1, 'cols':15}),
             'entry_fee': forms.TextInput(attrs={'rows':1, 'cols':15}),
             'place': forms.TextInput(attrs={'rows':1, 'cols':15}),
-            'capacity': forms.ChoiceField(choices=CAPACITY_CHOICES), #forms.TextInput(attrs={'rows':1, 'cols':15}),
+            'capacity': forms.ChoiceField(widget=forms.Select(choices=CAPACITY_CHOICES)), #forms.TextInput(attrs={'rows':1, 'cols':15}),
             'reg_deadline': forms.TextInput(attrs={'rows':1, 'cols':15}),
         }
         labels = {
