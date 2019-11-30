277d0f2d7f33d0b8f48e5c4e849499b755e373e9
Jan Vaculik <vaculikjan@gmail.com>
2019-11-30 14:37:30 +0100

    Prosím

--- IIS_project/Kulecnik/models.py
+++ IIS_project/Kulecnik/models.py
@@ -68,7 +68,12 @@ class Tournament_Teams(models.Model):
 
 class Tournament_S_referees(models.Model):
     tournament = models.ForeignKey(Tournament_S, on_delete=models.CASCADE)
-    referee = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    referee = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="referee_s")
+    registered = models.NullBooleanField(default=None)
+
+class Tournament_T_referees(models.Model):
+    tournament = models.ForeignKey(Tournament_T, on_delete=models.CASCADE)
+    referee = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="referee_t")
     registered = models.NullBooleanField(default=None)
 
 class Profile(models.Model):
