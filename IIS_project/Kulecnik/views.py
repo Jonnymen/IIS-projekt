036c21a29ffc4ac00ea7eac9a4d583b0c6e76dc9
Jan Menšík <jonny.mensik@gmail.com>
2019-11-30 15:32:22 +0100

    bug fix

--- IIS_project/Kulecnik/views.py
+++ IIS_project/Kulecnik/views.py
@@ -97,7 +97,7 @@ def tournament_detail_s(request, row_id):
     pocet = Tournament_Players.objects.filter(tournament=current_tournament, registered=True).count()
     zapasy = Game_S.objects.filter(tournament=current_tournament)
     rozhodci = Tournament_S_referees.objects.filter(tournament=current_tournament)
-    if request.user.is_authenticated():
+    if request.user.is_authenticated:
         if_referee = Tournament_S_referees.objects.filter(tournament=current_tournament, referee=request.user).count()
     else:
         if_referee = 0
