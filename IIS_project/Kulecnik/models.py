c3de7f200fba6140ba665f27a2620bdecef7ef31
Jan Vaculik <vaculikjan@gmail.com>
2019-11-30 18:54:52 +0100

    Test

--- IIS_project/Kulecnik/models.py
+++ IIS_project/Kulecnik/models.py
@@ -1,9 +1,9 @@
+from datetime import date
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User, AbstractUser
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from datetime import date
 from django.utils import timezone
 
 # Create your models here.
